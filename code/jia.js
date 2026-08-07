7ed4f8795d622dd7ba74462effea65abbba19d6a
Bogdan <Vavrencukbogdan28@gmail.com>
2026-08-07 15:12:24 +0300

    add

---
+++ code/jia.js
@@ -0,0 +1,27 @@
+var _____WB$wombat$assign$function_____=function(name){return (globalThis._wb_wombat && globalThis._wb_wombat.local_init && globalThis._wb_wombat.local_init(name))||globalThis[name];};if(!globalThis.__WB_pmw){globalThis.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opener = _____WB$wombat$assign$function_____("opener");
+
+}
+
+/*
+     FILE ARCHIVED ON 23:45:20 Aug 15, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 12:10:33 Aug 07, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 2.463
+  load_resource: 49.964
+  PetaboxLoader3.resolve: 35.189
+  PetaboxLoader3.datanode: 13.97
+*/
